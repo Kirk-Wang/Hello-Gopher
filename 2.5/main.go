eb59b8049d4c78ef82538e7cbf40e3a5a67f0be9
Kirk.Wang <kirk.w.wang@gmail.com>
2018-12-02 17:13:19 +0800

    training loop

---
+++ 2.5/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// 求mo &  / 2
+func convertToBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func forever() {
+	for {
+		fmt.Println("abc")
+	}
+}
+
+func main() {
+	fmt.Println(
+		convertToBin(5),
+		convertToBin(13),
+		convertToBin(72387885),
+		convertToBin(0),
+	)
+
+	printFile("abc.txt")
+}
--- main.go
+++ main.go
@@ -2,46 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 )
 
-func grade(score int) string {
-	g := ""
-	switch {
-	case score < 0 || score > 100:
-		panic(fmt.Sprintf("Wrong score: %d", score))
-	case score < 60:
-		g = "F"
-	case score < 80:
-		g = "C"
-	case score < 90:
-		g = "B"
-	case score <= 100:
-		g = "A"
-	}
-	return g
-}
-
 func main() {
-	const filename = "abc.txt"
-	// contents, err := ioutil.ReadFile(filename)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Printf("%s\n", contents)
-	// }
-	if contents, err := ioutil.ReadFile(filename); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("%s\n", contents)
-	}
-
-	fmt.Println(
-		grade(0),
-		grade(59),
-		grade(70),
-		grade(85),
-		grade(95),
-		grade(101),
-	)
+	fmt.Println("main")
 }

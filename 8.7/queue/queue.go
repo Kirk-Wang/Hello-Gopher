86f4d0fe1b2dcc1554b369c8db34755a644da8d0
Kirk.Wang <kirk.w.wang@gmail.com>
2018-12-09 15:16:30 +0800

    training

---
+++ 8.7/queue/queue.go
@@ -0,0 +1,22 @@
+package queue
+
+// An FIFO queue.
+type Queue []interface{}
+
+// Pushes the element into the queue.
+// e.g. q.Push(123)
+func (q *Queue) Push(v int) {
+	*q = append(*q, v)
+}
+
+// Pops elements from head.
+func (q *Queue) Pop() int {
+	head := (*q)[0]
+	*q = (*q)[1:]
+	return head.(int)
+}
+
+// Returns if the queue is empty or not.
+func (q *Queue) IsEmpty() bool {
+	return len(*q) == 0
+}

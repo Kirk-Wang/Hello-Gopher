611a2a08dee8f0d9b13ee3e1dcde7c2149fccdba
Kirk.Wang <kirk.w.wang@gmail.com>
2018-12-13 20:41:41 +0800

    training
    Signed-off-by: Kirk.Wang <kirk.w.wang@gmail.com>

---
+++ 14/crawler/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+	// 确认编码
+	e := determineEncoding(resp.Body)
+	// 转换编码
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", all)
+
+}
+
+// 确认页面编码
+func determineEncoding(r io.Reader) encoding.Encoding {
+	// 拿到前 1024 个 bytes
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Kirk-Wang/Hello-Gopher
 
-require github.com/golang/example v0.0.0-20170904185048-46695d81d1fa
+require (
+	github.com/golang/example v0.0.0-20170904185048-46695d81d1fa
+	golang.org/x/net v0.0.0-20181207154023-610586996380
+	golang.org/x/text v0.3.0
+)

cee945037f1039546950643eab6a063b70beb181
Kirk.Wang <kirk.w.wang@gmail.com>
2019-05-03 16:20:57 +0800

    update channel

---
+++ re/channel/done/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func doWorker(id int, in chan int, done chan bool) {
+	for n := range in {
+		fmt.Printf("worker %d recieved %c\n", id, n)
+		done <- true
+	}
+}
+
+type worker struct {
+	in   chan int
+	done chan bool
+}
+
+func createWorker(id int) worker {
+	w := worker{
+		in:   make(chan int),
+		done: make(chan bool),
+	}
+	go doWorker(id, w.in, w.done)
+	return w
+}
+
+func chanDemo() {
+	var workers [10]worker
+	for i := 0; i < 10; i++ {
+		workers[i] = createWorker(i)
+	}
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'a' + i
+		<-workers[i].done
+	}
+	for i := 0; i < 10; i++ {
+		workers[i].in <- 'A' + i
+		<-workers[i].done
+	}
+}
+
+func main() {
+	chanDemo()
+}

4d826b3e47bd61044a73990bed7e000c35191f82
Kirk.Wang <kirk.w.wang@gmail.com>
2019-05-03 16:58:10 +0800

    update channel/wait-group

--- re/channel/wait-group/main.go
+++ re/channel/wait-group/main.go
@@ -2,46 +2,48 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-func doWorker(id int, in chan int, done chan bool) {
-	for n := range in {
+func doWorker(id int, w worker) {
+	for n := range w.in {
 		fmt.Printf("worker %d recieved %c\n", id, n)
-		done <- true
+		w.done()
 	}
 }
 
 type worker struct {
 	in   chan int
-	done chan bool
+	done func()
 }
 
-func createWorker(id int) worker {
+func createWorker(id int, wg *sync.WaitGroup) worker {
 	w := worker{
-		in:   make(chan int),
-		done: make(chan bool),
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
 	}
-	go doWorker(id, w.in, w.done)
+	go doWorker(id, w)
 	return w
 }
 
 func chanDemo() {
+	var wg sync.WaitGroup
 	var workers [10]worker
+
 	for i := 0; i < 10; i++ {
-		workers[i] = createWorker(i)
+		workers[i] = createWorker(i, &wg)
 	}
+
+	wg.Add(20)
 	for i, worker := range workers {
 		worker.in <- 'a' + i
-	}
-	for _, worker := range workers {
-		<-worker.done
 	}
 	for i, worker := range workers {
 		worker.in <- 'A' + i
 	}
-	for _, worker := range workers {
-		<-worker.done
-	}
+	wg.Wait()
 }
 
 func main() {

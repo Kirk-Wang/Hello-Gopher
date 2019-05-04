9c1fbc4262c60b74d573945788b919b936cedd02
Kirk.Wang <kirk.w.wang@gmail.com>
2019-05-04 22:57:36 +0800

    update http

--- re/http/main.go
+++ re/http/main.go
@@ -10,7 +10,15 @@ func main() {
 	// resp, err := http.Get("https://lotteryjs.com")
 	req, _ := http.NewRequest(http.MethodGet, "https://www.imooc.com", nil)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
-	resp, err := http.DefaultClient.Do(req)
+	// resp, err := http.DefaultClient.Do(req)
+
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect:", req)
+			return nil
+		},
+	}
+	resp, err := client.Do(req)
 
 	if err != nil {
 		panic(err)

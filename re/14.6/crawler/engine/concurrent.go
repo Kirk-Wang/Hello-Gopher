0069b7acf47acae34368c4c6e78b0feb3d4e98ce
Kirk.Wang <kirk.w.wang@gmail.com>
2019-06-17 18:02:15 +0800

    update re

--- re/14.6/crawler/engine/concurrent.go
+++ re/14.6/crawler/engine/concurrent.go
@@ -1,13 +1,50 @@
 package engine
 
-type ConcurrentEngine struct{}
+import "fmt"
 
-type Scheduler interface{
+type ConcurrentEngine struct {
+	Scheduler   Scheduler
+	WorkerCount int
+}
+
+type Scheduler interface {
 	Submit(Request)
 }
 
-func (ConcurrentEngine) Run(seeds ...Request) {
+func (e ConcurrentEngine) Run(seeds ...Request) {
 	for _, r := range seeds {
-		scheduler.Submit(r)
+		e.Scheduler.Submit(r)
 	}
+
+	in := make(chan Request)
+	out := make(chan ParseResult)
+
+	for i := 0; i < e.WorkerCount; i++ {
+		createWorker(in, out)
+	}
+
+	for {
+		result := <-out
+
+		for _, item := range result.Items {
+			fmt.Printf("Got item: %v", item)
+		}
+
+		for _, request := range result.Requests {
+			e.Scheduler.Submit(request)
+		}
+	}
+}
+
+func createWorker(in chan Request, out chan ParseResult) {
+	go func() {
+		for {
+			request := <-in
+			parseResult, err := worker(request)
+			if err != nil {
+				continue
+			}
+			out <- parseResult
+		}
+	}()
 }
--- re/14.6/crawler/engine/simple.go
+++ re/14.6/crawler/engine/simple.go
@@ -16,7 +16,7 @@ func (e SimpleEngine) Run(seeds ...Request) {
 		r := requests[0]
 		requests = requests[1:]
 
-		parseResult, err := e.worker(r)
+		parseResult, err := worker(r)
 		if err != nil {
 			continue
 		}
@@ -28,7 +28,7 @@ func (e SimpleEngine) Run(seeds ...Request) {
 	}
 }
 
-func (SimpleEngine) worker(r Request) (ParseResult, error) {
+func worker(r Request) (ParseResult, error) {
 	log.Printf("Fetching %s", r.Url)
 	body, err := fetcher.Fetch(r.Url)
 	if err != nil {

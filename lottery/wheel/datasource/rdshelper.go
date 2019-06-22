d3e9473047c152db49c46f9bb44bf36c71c0247e
Kirk.Wang <kirk.w.wang@gmail.com>
2019-06-22 09:27:11 +0800

    update lottery

--- lottery/wheel/datasource/rdshelper.go
+++ lottery/wheel/datasource/rdshelper.go
@@ -1,7 +1,10 @@
 package datasource
 
 import (
+	"fmt"
+	"log"
 	"sync"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
@@ -18,4 +21,21 @@ func (rds *RedisConn) Do(commandName string,
 	args ...interface{}) (reply interface{}, err error) {
 	conn := rds.pool.Get()
 	defer conn.Close()
+
+	t1 := time.Now().UnixNano()
+	reply, err = conn.Do(commandName, args...)
+
+	if err != nil {
+		e := conn.Err()
+		if e != nil {
+			log.Println("rdshelper.Do", err, e)
+		}
+	}
+
+	t2 := time.Now().UnixNano()
+	if rds.showDebug {
+		fmt.Printf("[redis] [info] [%dus]cmd=%s, err=%s, args=%v, reply=%s\n",
+			(t2-t1)/1000, commandName, err, args, reply)
+	}
+	return reply, err
 }

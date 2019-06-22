a520d6645746f097b9a4ea6f733ad0c00e1cc9c3
Kirk.Wang <kirk.w.wang@gmail.com>
2019-06-22 09:57:17 +0800

    update

--- lottery/wheel/datasource/rdshelper.go
+++ lottery/wheel/datasource/rdshelper.go
@@ -6,14 +6,16 @@ import (
 	"sync"
 	"time"
 
+	"github.com/Kirk-Wang/Hello-Gopher/lottery/wheel/conf"
+
 	"github.com/gomodule/redigo/redis"
 )
 
 var rdsLock sync.Mutex
-var cacheInstance *redis.Conn
+var cacheInstance *RedisConn
 
 type RedisConn struct {
-	pool      *redis.pool
+	pool      *redis.Pool
 	showDebug bool
 }
 
@@ -38,4 +40,53 @@ func (rds *RedisConn) Do(commandName string,
 			(t2-t1)/1000, commandName, err, args, reply)
 	}
 	return reply, err
+}
+
+func (rds *RedisConn) ShowDebug(b bool) {
+	rds.showDebug = b
+}
+
+func InstanceCache() *RedisConn {
+	if cacheInstance != nil {
+		return cacheInstance
+	}
+	rdsLock.Lock()
+	defer rdsLock.Unlock()
+
+	if cacheInstance != nil {
+		return cacheInstance
+	}
+	return NewCache()
+}
+
+func NewCache() *RedisConn {
+	pool := redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp",
+				fmt.Sprintf("%s:%d", conf.RdsCache.Host, conf.RdsCache.Port))
+			if err != nil {
+				log.Fatal("rdshelper.NewCache Dial error=", err)
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+		MaxIdle:         10000,
+		MaxActive:       10000,
+		IdleTimeout:     0,
+		Wait:            false,
+		MaxConnLifetime: 0,
+	}
+	instance := &RedisConn{
+		pool: &pool,
+	}
+	cacheInstance = instance
+	instance.ShowDebug(true)
+	return instance
 }

12c1612809e48cfdda4fdda2ceb16305c5d4dec7
Kirk.Wang <kirk.w.wang@gmail.com>
2019-06-22 14:27:35 +0800

    update wheel

--- lottery/wheel/bootstrap/bootstrapper.go
+++ lottery/wheel/bootstrap/bootstrapper.go
@@ -1,6 +1,8 @@
 package	bootstrap
 
 import (
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
 	"github.com/kataras/iris"
 )
 
@@ -40,4 +42,57 @@ func (b *Bootstrapper) SetupViews(viewDir string) {
 		return dt.Format(conf.SysTimeform)
 	})
 	b.RegisterView(htmlEngine)
+}
+
+func (b *Bootstrapper) SetupErrorHandlers() {
+	b.OnAnyErrorCode(func(ctx iris.Context) {
+		err := iris.Map{
+			"app": b.AppName,
+			"status": ctx.GetStatusCode(),
+			"message": ctx.Values().GetString("message"),
+		}
+
+		if jsonOutput := ctx.URLParamExists("json"); jsonOutput {
+			ctx.JSON(err)
+			return
+		}
+
+		ctx.ViewData("Err", err)
+		ctx.ViewData("Title", "Error")
+		ctx.View("shared/error.html")
+	})
+}
+
+func (b *Bootstrapper) Configure(cs ...Configurator) {
+	for _, c := range cs {
+		c(b)
+	}
+}
+
+func (b *Bootstrapper) setupCron() {
+	// TODO:
+}
+
+const (
+	StaticAssets = "./public"
+	Favicon = "favicon.ico"
+)
+
+func (b *Bootstrapper) Bootstrap() *Bootstrapper{
+	b.SetupViews("./views")
+	b.SetupErrorHandlers()
+
+	b.Favicon(StaticAssets + Favicon)
+	b.StaticWeb(StaticAssets[1:len(StaticAssets)-1], StaticAssets)
+
+	b.setupCron()
+
+	b.Use(recover.New())
+	b.Use(logger.New())
+
+	return b
+}
+
+func (b *Bootstrapper) Listen(addr string, cfgs ...iris.Configurator) {
+	b.Run(iris.Addr(addr), cfgs...)
 }

f33830dd849ca3e547576ffbf46232b66640d947
Kirk.Wang <kirk.w.wang@gmail.com>
2019-06-24 16:39:47 +0800

    update

--- resk/demo/json.go
+++ resk/demo/json.go
@@ -13,15 +13,23 @@ type User struct {
 }
 
 func main() {
-	u := User{
-		Id:      12,
-		Name:    "wendell",
-		Age:     1,
-		Address: "成都高新区",
-	}
-	data, err := json.Marshal(&u)
+	// u := User{
+	// 	Id:      12,
+	// 	Name:    "wendell",
+	// 	Age:     1,
+	// 	Address: "成都高新区",
+	// }
+	// data, err := json.Marshal(&u)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(string(data))
+
+	data := []byte(`{"id":"12","username":"wendell","age":1}`)
+	u2 := &User{}
+	err := json.Unmarshal(data, u2)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(data))
+	fmt.Printf("%+v \n", u2)
 }

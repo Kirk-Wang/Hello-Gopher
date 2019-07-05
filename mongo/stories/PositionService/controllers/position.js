bb6fca19416f414bf211e2f0d7610d17959976c8
Kirk.Wang <kirk.w.wang@gmail.com>
2019-07-05 16:11:49 +0800

    update

---
+++ mongo/stories/PositionService/controllers/position.js
@@ -0,0 +1,43 @@
+var Postion = require("../models/position")
+
+// Create
+exports.createPosition = function(req, res, next) {
+  var position = new Position(
+    {
+      account: req.body.account,
+      stock: req.body.stock,
+      quantity: req.body.quantity,
+      price: req.body.price
+    }
+  )
+  position.save(function(err) {
+    if (err) {
+      return next(err)
+    }
+    res.send('仓位纪录添加成功')
+  })
+}
+
+// Read
+exports.queryPosition = function(req, res, next) {
+  Postion.find({
+    account: req.params.account
+  }, function(err, position) {
+    if (err) {
+      return next(err)
+    }
+    res.send(position)
+  })
+}
+
+//Update
+exports.queryPosition = function(req, res, next) {
+  Postion.findByIdAndUpdate(req.params.id, {
+    $set: req.body
+  }, function(err) {
+    if (err) {
+      return next(err)
+    }
+    res.send('仓位纪录更新成功')
+  })
+}

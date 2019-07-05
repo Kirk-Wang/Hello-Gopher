3937024861e7a27cdbd842abdd0cb1b0ce354060
Kirk.Wang <kirk.w.wang@gmail.com>
2019-07-05 15:56:37 +0800

    update

---
+++ mongo/stories/PositionService/models/position.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongose")
+var Schema = mongoose.Schema;
+
+var PositionSchema = new Schema({
+  account: {
+    type: String,
+    require: true,
+  },
+  stock: {
+    type: String,
+    require: true,
+  },
+  quantity: {
+    type: Number,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+})
+
+module.exports = mongoose.model("Position", PositionSchema)
+

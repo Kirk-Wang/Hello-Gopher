499492cbfbed8384446f36fd87c74ff80e3cde39
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-18 13:23:29 +0800

    update

--- vanilla/maze/maze.go
+++ vanilla/maze/maze.go
@@ -23,13 +23,94 @@ func readMaze(filename string) [][]int {
 	return maze
 }
 
-func walk(maze [][]int, start, end) {
+type point struct {
+	i, j int
+}
 
+// 上左下右
+var dirs = [4]point{
+	{-1, 0},
+	{0, -1},
+	{1, 0},
+	{0, 1},
+}
+
+func (p point) add(r point) point {
+	return point{
+		p.i + r.i,
+		p.j + r.j,
+	}
+}
+
+func (p point) at(grid [][]int) (int, bool) {
+	// 上，下越界
+	if p.i < 0 || p.i >= len(grid) {
+		return 0, false
+	}
+
+	// 左，右越界
+	// p.i 是 row
+	if p.j < 0 || p.j >= len(grid[p.i]) {
+		return 0, false
+	}
+
+	return grid[p.i][p.j], true
+}
+
+func walk(maze [][]int, start, end point) [][]int {
+	steps := make([][]int, len(maze))
+	for i := range steps {
+		steps[i] = make([]int, len(maze[i]))
+	}
+	Q := []point{start}
+	for len(Q) > 0 {
+		cur := Q[0]
+		Q = Q[1:]
+		if cur == end {
+			// 发现终点了
+			break
+		}
+		for _, dir := range dirs {
+			next := cur.add(dir)
+
+			// maze at next is 0
+			// and steps at next is 0
+			// and next != start 不能回到起点
+			val, ok := next.at(maze)
+			if !ok || val == 1 {
+				continue
+			}
+
+			val, ok = next.at(steps)
+			if !ok || val != 1 {
+				continue
+			}
+
+			if next == start {
+				continue
+			}
+			curSteps, _ := cur.at(steps)
+			steps[next.i][next.j] = curSteps + 1
+
+			Q = append(Q, next)
+		}
+	}
+	return steps
 }
 
 func main() {
 	maze := readMaze("maze.in")
 	for _, row := range maze {
+		for _, val := range row {
+			fmt.Printf("%d ", val)
+		}
+		fmt.Println()
+	}
+
+	steps := walk(maze, point{0, 0},
+		point{len(maze) - 1, len(maze[0]) - 1})
+
+	for _, row := range steps {
 		for _, val := range row {
 			fmt.Printf("%d ", val)
 		}

f0b11fcf2f71fa710448a2abb7318941f6ab31d4
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-18 12:08:08 +0800

    update

---
+++ vanilla/maze/maze.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func readMaze(filename string) [][]int {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	var row, col int
+	fmt.Fscanf(file, "%d %d", &row, &col)
+
+	maze := make([][]int, row)
+	for i := range maze {
+		maze[i] = make([]int, col)
+		for j := range maze[i] {
+			fmt.Fscanf(file, "%d", &maze[i][j])
+		}
+	}
+	return maze
+}
+
+func main() {
+	maze := readMaze("maze.in")
+	for _, row := range maze {
+		for _, val := range row {
+			fmt.Printf("%d ", val)
+		}
+		fmt.Println()
+	}
+}

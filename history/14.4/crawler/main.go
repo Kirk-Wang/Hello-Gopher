b6311d4d2278919c8853542a2b1df9addf8f6463
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-30 12:31:28 +0800

    update

--- history/14.4/crawler/main.go
+++ history/14.4/crawler/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/transform"
-	"io"
 	"io/ioutil"
 	"net/http"
 	"regexp"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
 )
 
 func main() {
@@ -23,10 +23,9 @@ func main() {
 		fmt.Println("Error: status code", resp.StatusCode)
 		return
 	}
-	// 确认编码
-	e := determineEncoding(resp.Body)
-	// 转换编码
-	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	bodyReader := bufio.NewReader(resp.Body)
+	e := determineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
 	all, err := ioutil.ReadAll(utf8Reader)
 	if err != nil {
 		panic(err)
@@ -36,7 +35,7 @@ func main() {
 }
 
 // 确认页面编码
-func determineEncoding(r io.Reader) encoding.Encoding {
+func determineEncoding(r *bufio.Reader) encoding.Encoding {
 	// 拿到前 1024 个 bytes
 	bytes, err := bufio.NewReader(r).Peek(1024)
 	if err != nil {
@@ -48,9 +47,13 @@ func determineEncoding(r io.Reader) encoding.Encoding {
 
 func printCityList(contents []byte) {
 	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`)
+	// matches := re.FindAll(contents, -1) // [][]byte -> []byte 相当于 string
 	matches := re.FindAllSubmatch(contents, -1)
 	for _, m := range matches {
-		fmt.Printf("City: %s, URL: %s\n", m[2], m[1])
+		// for _, subMatch := range m {
+		// 	// fmt.Printf("%s ", subMatch)
+		// }
+		fmt.Printf("City: %s, URL: %s", m[2], m[1])
 		fmt.Println()
 	}
 	fmt.Printf("Matches found: %d\n", len(matches))

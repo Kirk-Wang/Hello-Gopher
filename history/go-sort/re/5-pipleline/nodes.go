43442b5c585d8468fd89d3549a5d7448b110de75
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-31 20:17:37 +0800

    update

--- history/go-sort/re/5-pipleline/nodes.go
+++ history/go-sort/re/5-pipleline/nodes.go
@@ -1,6 +1,8 @@
 package pipleline
 
-func ArraySource(a ...int) chan int {
+func ArraySource(a ...int) <-chan int {
+	// <-chan int: 表示用它的人只能从里面拿东西
+	// 在这里我们只能放东西
 	out := make(chan int)
 	go func() {
 		for _, v := range a {
@@ -10,4 +12,15 @@ func ArraySource(a ...int) chan int {
 		close(out)
 	}()
 	return out
+}
+
+// in 是节点的一个输入
+// return 的 channel 是一个输出
+// return 是相对于使用者的来说
+// 使用的人只能用它去收东西
+// 对于 InMemSort 只能发东西
+// in <-chan int::只进不出的 channel
+// 返回的是  <-chan int：只出不进的 channel
+func InMemSort(in <-chan int) <-chan int {
+
 }

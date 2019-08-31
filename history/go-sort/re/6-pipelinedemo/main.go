bbfec1a09ba5c2b3422302174f83eb7048cccc0c
Kirk.Wang <kirk.w.wang@gmail.com>
2019-08-31 20:03:21 +0800

    update

---
+++ history/go-sort/re/6-pipelinedemo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	pipleline "github.com/Kirk-Wang/Hello-Gopher/history/go-sort/re/5-pipleline"
+)
+
+func main() {
+	p := pipleline.ArraySource(3, 2, 6, 7, 4)
+	for {
+		if num, ok := <-p; ok {
+			fmt.Println(num)
+		} else {
+			// chanel 关闭了
+			break
+		}
+	}
+}
